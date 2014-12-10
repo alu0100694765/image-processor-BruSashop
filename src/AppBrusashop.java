d592c09bb43e0b02924ce17fef7ded555f9ff9c9
Sawan J.Kapai Harpalani <alu0100694765@ull.edu.es>
2014-12-10 11:18:41 +0000

    Added Look N Feel

--- src/AppBrusashop.java
+++ src/AppBrusashop.java
@@ -1,5 +1,8 @@
 import java.io.IOException;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 import modelo.ManipuladorImagenes;
 import vista.InterfazGrafica;
 import controlador.Controlador;
@@ -26,8 +29,17 @@ public class AppBrusashop {
 	 *
 	 * @param args the arguments
 	 * @throws IOException Signals that an I/O exception has occurred.
+	 * @throws UnsupportedLookAndFeelException 
+	 * @throws IllegalAccessException 
+	 * @throws InstantiationException 
+	 * @throws ClassNotFoundException 
 	 */
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
+		
+		// Look And Feel
+		UIManager.setLookAndFeel("com.jtattoo.plaf.texture.TextureLookAndFeel");
+		
+		
 		// TODO Auto-generated method stub
 		InterfazGrafica grafica = new InterfazGrafica();
 		ManipuladorImagenes imagenes = new ManipuladorImagenes();
