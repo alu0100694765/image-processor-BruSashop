d019ef1140247c5dbb32d069ee101539c120f3de
Sawan J.Kapai Harpalani <alu0100694765@ull.edu.es>
2014-12-10 11:49:01 +0000

    Added icon

--- src/vista/InterfazGrafica.java
+++ src/vista/InterfazGrafica.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
@@ -237,7 +238,9 @@ public class InterfazGrafica {
 	public InterfazGrafica() {
 
 		frame = new JFrame(TITULO);
-
+		
+		frame.setIconImage(new ImageIcon("resources/icon.png").getImage());
+        
 		// Inicializa la barra del menu
 		barra_acciones = new JMenuBar();
 
